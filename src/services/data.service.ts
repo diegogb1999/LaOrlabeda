77d0502aced26d84a0fbae923e12866c040c2fc1
diegogb1999 <diegogb1999@gmail.com>
2024-02-10 14:47:30 +0100

    Pre-cookies

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,26 +12,26 @@ export class DataService {
   private nodo = "usuarios";
   //private apiUrl = 'https://tu-api.com/datos';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient,  private authService: AuthService) {
 
   }
 
   agregarDatos(datos: any): Observable<any> {
-    return this.http.post(`${this.firebaseUrl}/${this.nodo}.json`, datos);
+    return this.http.post(`${this.firebaseUrl}/${this.nodo}.json?auth=${this.authService.getToken}`, datos);
   }
 
   obtenerDatos(): Observable<any> {
-    return this.http.get(`${this.firebaseUrl}/${this.nodo}.json`);
+    return this.http.get(`${this.firebaseUrl}/${this.nodo}.json?auth=${this.authService.getToken}`);
   }
 
   actualizarDatos(id: string, datosActualizados: any): Observable<any> {
-    const url = `${this.firebaseUrl}${this.nodo}/${id}.json`;
+    const url = `${this.firebaseUrl}${this.nodo}/${id}.json?auth=${this.authService.getToken}`;
     return this.http.put<any>(url, datosActualizados);
   }
 
   eliminarRegistro(id: string): Observable<void> {
-    const url = `${this.firebaseUrl}${this.nodo}${id}.json`;
+    const url = `${this.firebaseUrl}${this.nodo}${id}.json?auth=${this.authService.getToken}`;
     return this.http.delete<void>(url);
     }
-    
+
 }
